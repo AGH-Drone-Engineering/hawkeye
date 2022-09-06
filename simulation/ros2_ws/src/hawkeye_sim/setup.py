59ec7e645a6c487532805edd854e2bd8a9d90943
Carbon225 <carbon225@protonmail.com>
2022-09-06 19:35:15 +0200

    remove gods eye

--- simulation/ros2_ws/src/hawkeye_sim/setup.py
+++ simulation/ros2_ws/src/hawkeye_sim/setup.py
@@ -24,7 +24,6 @@ setup(
     entry_points={
         'console_scripts': [
             'gods_hand = hawkeye_sim.gods_hand:main',
-            'gods_eye = hawkeye_sim.gods_eye:main',
             'logical_camera = hawkeye_sim.logical_camera:main',
         ],
     },
